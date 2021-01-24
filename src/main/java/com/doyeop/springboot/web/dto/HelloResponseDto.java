c5f3a34358bcee4a5f7bdc3a210af467a7ab5651
Doyeop Kwon <kwon32220@gmail.com>
2021-01-24 17:00:24 +0900

    chapter2

---
+++ src/main/java/com/doyeop/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.doyeop.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
